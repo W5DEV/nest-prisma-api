7a3b7e214ff19d51f5e3cd67c06f577f1bcce00d
John Larson <jl@buildmidwestern.com>
2023-08-06 12:46:58 -0500

    chore: added nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -11,6 +12,8 @@ import { SystemRolesModule } from './system-roles/system-roles.module';
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [PrismaModule, UsersModule, RecipesModule, IngredientsModule, InstructionsModule, SystemRolesModule],
+  imports: [PrismaModule, UsersModule, RecipesModule, IngredientsModule, InstructionsModule, SystemRolesModule, ConfigModule.forRoot({
+    isGlobal: true,
+  })],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {ValidationPipe} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
 
-  if (process.env.NODE_ENV !== 'production'){
+  if (configService.get('NODE_ENV') !== 'production'){
     const config = new DocumentBuilder()
         .setTitle('Recipe Vault API')
         .setDescription('Recipe Vault API Documentation')
